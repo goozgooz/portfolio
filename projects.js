b16b8136a070419d282353716fcb7252f34b1c11
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-07-30 09:11:29 -0700

    social media links open in new tab + JS construcotr made

---
+++ projects.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function Project(allProjects){
+  this.projectName = allProjects.projectName;
+  this.description = allProjects.description;
+  this.picture = allProjects.picture;
+};
