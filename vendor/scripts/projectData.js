fbecd9943bbc33c0648f9214b67fa99f6b4021ae
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-07-30 09:54:38 -0700

    added project data

---
+++ vendor/scripts/projectData.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var allProjects = [
+  {
+    projectName:      'Bacon Ipsum',
+    description:  'Basic About Me Page and Quiz',
+    code:         'https://github.com/goozgooz/About-Me-1',
+    picture:      '',
+  },
+  {
+    projectName:  'Pat\'s Salmon Cookies',
+    description:  'Designed a page for a very weird and thankfully theoretical Salmon Cookie Baker',
+    code:         'https://github.com/goozgooz/cookie-stand',
+    picture:      '',
+  },
+  {
+    projectName:  'Bus Mall Survery',
+    description:  'Created a product survey and gathered data and then presented the data so that the company could decide which products to sell in their upcoming catalog',
+    code:         'https://github.com/goozgooz/bus-mall',
+    picture:       '',
+  },
+  {
+    projectName:  'Unicorn Toilet',
+    description:  'Final Project for 201 Course',
+    code:         'https://github.com/goozgooz/bus-mall',
+    picture:      '',
+  },
+];
---
+++ vendor/scripts/projects.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function Project(allProjects){
+  this.projectName = allProjects.projectName;
+  this.description = allProjects.description;
+  this.code = allProjects.code;
+  this.picture = allProjects.picture;
+};
