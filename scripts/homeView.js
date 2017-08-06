ef41c8eb86923d018054ffd02bf82e3483503381
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-08-06 16:59:16 -0700

    tab functionality working in desktop. working on mobile

--- scripts/homeView.js
+++ scripts/homeView.js
@@ -2,6 +2,11 @@
 
 var homePage = {};
 
+homePage.load = function() {
+  $('.tab-content').hide();
+  $('.about-me').show();
+};
+
 homePage.navBar = function() {
   $('.hamburger-menu').hover(
     function(){
@@ -12,4 +17,14 @@ homePage.navBar = function() {
     });
 };
 
+homePage.tabs = function() {
+  $('.nav-bar').on('click', 'li', function(e) {
+    e.preventDefault();
+    $('.tab-content').hide();
+    $('.' + $(this).data('attribute')).fadeIn();
+  });
+};
+
+homePage.load();
 homePage.navBar();
+homePage.tabs();
