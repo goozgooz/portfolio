af6bb5966e94bf630e39704c74e6941da7c533a8
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-09-08 20:41:37 -0700

    moved token

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
+const sa = require('superagent');
 
 var express = require('express');
 var app = express();
@@ -10,6 +11,15 @@ app.use(express.static('./public'));
 app.post('/articles', bodyParser, function(request, response) {
   console.log(request.body);
   response.send('Record posted to server!!');
+})
+
+app.get('/repos', (req, res) => {
+  sa.get('https://api.github.com/user/repos')
+    .set('Authorization', `token ${process.env.GITHUB_TOKEN}`)
+    .end((err,results) => {
+      if(err) console.log(err);
+      res.send(result.body);
+    })
 })
 
 app.listen(PORT, function() {
